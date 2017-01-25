dd17e7aa0c715177090e050744e0e32f9929ffce
Joseba Legarreta <josebalega@gmail.com>
2017-01-25 16:28:54 -0500

    Show text

--- taggy.js
+++ taggy.js
@@ -1,4 +1,4 @@
-// Taggy 0.0.2
+// Taggy 0.0.3
 
 (function() {
 
@@ -7,11 +7,11 @@
       var selector = document.querySelectorAll(params);
 
       this.length = selector.length;
-      this.version = '0.0.2';
+      this.version = '0.0.3';
 
       for(var i = 0; i < this.length; i++) {
         this[i] = selector[i];
-        var divId = _taggy.generateId();
+        var divId = _taggy.generateId('taggy-container');
         div = document.createElement('div');
         this[i].divId = divId;
         div.setAttribute('id', divId);
@@ -33,8 +33,8 @@
 
   _taggy.counter = 0;
 
-  _taggy.generateId = function(){
-    return 'taggy-id-' + this.counter++;
+  _taggy.generateId = function(prefix){
+    return prefix + '-' + this.counter++;
   };
 
   _taggy.fn = Taggy.prototype = {
@@ -62,9 +62,9 @@
         data.forEach(function(coords){
           var span = document.createElement('span');
           span.setAttribute('class', 'taggy-coord');
-          if(coords.id) span.setAttribute('id', coords.id);
           var styles = 'left: ' + coords.x + '%;top: ' + coords.y + '%;';
           span.setAttribute('style', styles);
+          if(typeof coords.options === 'object') span = __parseOptions(span, coords, div);
           div.appendChild(span);
         });
       }
@@ -79,6 +79,28 @@
     var relativeX = (absoluteX * 100) / this.offsetWidth;
     var relativeY = (absoluteY * 100) / this.offsetHeight;
     return {x: relativeX, y: relativeY};
+  };
+
+  var __parseOptions = function(span, coords, div){
+    var options = coords.options;
+    if(options.id) span.setAttribute('id', options.id);
+    if(options.class) span.className += ' ' + options.class;
+    if(typeof options.text === 'string'){
+      var type = options.type === 'get' ? 'get' : 'set';
+      span.dataset.id = _taggy.generateId('tag');
+      if(type === 'set'){
+        // TODO: Return an input and check if the val is correct
+      }else{
+        // TODO: Show an audio or a image? Maybe this should be a plugin (.fn)
+        span.addEventListener('click', function(event){
+          span.innerHTML = options.text;
+          span.style['margin-top'] = '-30px';
+          span.style['margin-left'] = '-20px';
+          span.style.background = 'none';
+        });
+      }
+    }
+    return span;
   };
 
   if(!window.Taggy) window.Taggy = _taggy;
